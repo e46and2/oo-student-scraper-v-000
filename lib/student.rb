6717aeae7b71b45b5103fc9bc0c09afeb92bb544
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-04-19 22:55:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,8 @@ class Student
     @profile_quote = profile_quote
     @bio = bio
     @profile_url = profile_url
+
+    @@all << self 
   end
 
   def self.create_from_collection(students_array)
@@ -25,6 +27,6 @@ class Student
   end
 
   def self.all
-    @@all 
+    @@all
   end
 end

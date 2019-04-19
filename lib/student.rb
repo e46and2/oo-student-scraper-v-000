4a68e9ffb5ffae0bb33312bab43ec85cb8b3eb2b
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-04-19 23:36:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,8 +23,8 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |k, v|
-        self.send(("#{k}="), v)
+    attributes_hash.each do |key, value|
+        self.send(("#{key}="), value)
       end
     self
   end

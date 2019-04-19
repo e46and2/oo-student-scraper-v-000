9e67be629c7f6dc001dfaa171ed7b9f14728a13f
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-04-19 22:39:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -47,7 +47,6 @@ class Scraper
       student_profile[:quote] = page.css(".profile-quote").text
       student_profile[:bio] = page.css("div.description-holder p").text
       student_profile
-
   end
 
 end

f7227fb09145bb89c94a652a1086c8b694c27c64
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-04-19 23:29:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,12 +17,12 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    self.new 
   end
 
   def add_student_attributes(attributes_hash)
 
-    self.student 
+    self.student
   end
 
   def self.all
